ce047559ad2d18952bfd1e0020fdaa139bf95bc4
Prateek Kumar <prateekkumartiwari01@gmail.com>
2022-01-24 22:00:30 +0530

    Added Required Middlewares.

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,16 @@ const app = express();
 const database = require("./configuration/databaseConfiguration");
 const dotenv = require("dotenv");
 dotenv.config({path : "./configuration/configuration.env"});
+const videoRouter = require("./routes/videoRoutes");
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true})); // used for forms like the one I made for smllr ( where I send code and other details under different keys )
+app.use("/api/v1/video",videoRouter);
 
 
 database()
     .then(()=>console.log("Connected To Database"))
-    .catch((error)=>console.log("An Error occurred While Connecting To Database"));
+    .catch(()=>console.log("An Error occurred While Connecting To Database"));
 
 
 const PORT = 8000 || process.env.PORT;
