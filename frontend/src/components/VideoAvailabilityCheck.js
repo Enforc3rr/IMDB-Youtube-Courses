1bc076fc667c6c770e98b37db98b41e94a3cc74c
Prateek Kumar <prateekkumartiwari01@gmail.com>
2022-01-28 14:52:04 +0530

    Connected Details And Availability Check.js

--- frontend/src/components/VideoAvailabilityCheck.js
+++ frontend/src/components/VideoAvailabilityCheck.js
@@ -1,10 +1,12 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import VideoDetailsForm from "./VideoDetailsForm";
 
 function VideoAvailabilityCheck() {
   const [videoPresent, setVideoPresent] = useState(null);
   const [initialCheck, setInitialCheck] = useState(false);
   const [videoUrl, setVideoUrl] = useState("");
+  const [next, setNext] = useState(false);
 
   const checkVideoAvailabilty = async () => {
     setInitialCheck(true);
@@ -20,46 +22,60 @@ function VideoAvailabilityCheck() {
     }
   };
 
+  const videoDetails = () => {
+    setNext(true);
+  };
   return (
-    <div
-      className="container d-flex justify-content-center align-items-center"
-      style={{ border: "2px solid black", minHeight: "100vh" }}
-    >
-      <div className="form-group row shadow-lg" style={{ width: "100%" }}>
-        <div className="text-center col-sm-12 col-md-12 col-lg-12">
-          <h2 className="display-4">
-            <u>Add Video Details</u>
-          </h2>
+    <div>
+      {!next ? (
+        <div
+          className="container d-flex justify-content-center align-items-center"
+          style={{ border: "2px solid black", minHeight: "100vh" }}
+        >
+          <div className="form-group row shadow-lg" style={{ width: "100%" }}>
+            <div className="text-center col-sm-12 col-md-12 col-lg-12">
+              <h2 className="display-4">
+                <u>Add Video Details</u>
+              </h2>
+            </div>
+            <div className="col-sm-12 col-md-4 col-lg-4 pl-5 pt-1">
+              <label htmlFor="youtubeLink" className="display-4">
+                Video Link
+              </label>
+            </div>
+            <div className="col-sm-12 col-md-8 col-lg-8 mt-2">
+              <input
+                type="text"
+                id="youtubeLink"
+                className="form-control"
+                style={{ height: "75%" }}
+                value={videoUrl}
+                onChange={(e) => setVideoUrl(e.target.value)}
+              />
+            </div>
+            <div className="col-12 text-center">
+              {!initialCheck ? (
+                <button
+                  className="btn btn-primary"
+                  onClick={checkVideoAvailabilty}
+                >
+                  Check Availability
+                </button>
+              ) : !videoPresent ? (
+                <button className="btn btn-green" onClick={videoDetails}>
+                  Next
+                </button>
+              ) : (
+                <button className="btn btn-danger" disabled>
+                  Video is already present
+                </button>
+              )}
+            </div>
+          </div>
         </div>
-        <div className="col-sm-12 col-md-4 col-lg-4 pl-5 pt-1">
-          <label htmlFor="youtubeLink" className="display-4">
-            Video Link
-          </label>
-        </div>
-        <div className="col-sm-12 col-md-8 col-lg-8 mt-2">
-          <input
-            type="text"
-            id="youtubeLink"
-            className="form-control"
-            style={{ height: "75%" }}
-            value={videoUrl}
-            onChange={(e) => setVideoUrl(e.target.value)}
-          />
-        </div>
-        <div className="col-12 text-center">
-          {!initialCheck ? (
-            <button className="btn btn-primary" onClick={checkVideoAvailabilty}>
-              Check Availability
-            </button>
-          ) : !videoPresent ? (
-            <button className="btn btn-green">Next</button>
-          ) : (
-            <button className="btn btn-danger" disabled>
-              Video is already present
-            </button>
-          )}
-        </div>
-      </div>
+      ) : (
+        <VideoDetailsForm videoUrl={`${videoUrl}`} />
+      )}
     </div>
   );
 }
