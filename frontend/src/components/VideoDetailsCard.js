86042a69b21c4a973691076a301c0f3d187b0d93
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-01-31 19:50:55 +0530

    Implemented Skeleton Card Loading effect.

--- frontend/src/components/VideoDetailsCard.js
+++ frontend/src/components/VideoDetailsCard.js
@@ -24,7 +24,20 @@ function VideoDetailsCard(props) {
 
   const skeletonLoadingEffect = () => {
     return (
-      
+      <div className="ph-item">
+        <div className="ph-col-12">
+          <div className="ph-picture"></div>
+          <div className="ph-row">
+            <div className="ph-col-12 big"></div>
+            <div className="ph-col-12 empty big"></div>
+            <div className="ph-col-12"></div>
+            <div className="ph-col-12"></div>
+            <div className="ph-col-12"></div>
+            <div className="ph-col-12 empty big"></div>
+            <div className="ph-col-12 big"></div>
+          </div>
+        </div>
+      </div>
     );
   };
 
