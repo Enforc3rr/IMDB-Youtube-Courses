76a42062f1e61d24ea54e61ccb75c275fff396bc
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-01-31 19:48:26 +0530

    Added Details to VideoDetailsCard.js

--- frontend/src/components/VideoDetailsCard.js
+++ frontend/src/components/VideoDetailsCard.js
@@ -1,41 +1,80 @@
-function VideoDetailsCard() {
+import { useState, useEffect } from "react";
+import { Rating as ReactStars } from "react-simple-star-rating";
+
+function VideoDetailsCard(props) {
+  const [isPageLoading, setIsPageLoading] = useState(true);
+  const [title, setTitle] = useState("");
+  const [uploadedBy, setUploadedBy] = useState("");
+  const [totalRating, setTotalRating] = useState(0);
+  const [topic, setTopic] = useState("");
+  const [thumbnail, setThumbnail] = useState("");
+  const [videoLink, setVideoLink] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTitle(props.data.videoTitle);
+      setThumbnail(props.data.videoThumbnailURL);
+      setUploadedBy(props.data.videoUploadedBy);
+      setTotalRating(props.data.videoRating);
+      setVideoLink(`https://www.youtube.com/watch?v=${props.data.videoID}`);
+      setTopic(props.data.videoTopic);
+      setIsPageLoading(false);
+    }, 1000);
+  }, []);
+
+  const skeletonLoadingEffect = () => {
+    return (
+      
+    );
+  };
+
   return (
-    <div className="row">
-      <div className="col-sm-12 col-lg-4 col-md-6">
-        <div className="card shadow m-3">
-          <div className="card-header text-center">
+    <div className="container">
+      {isPageLoading ? (
+        skeletonLoadingEffect()
+      ) : (
+        <div className="card shadow m-3" style={{ height: "75vh" }}>
+          <div className="card-header text-center p-2">
             <img
-              src="https://mdbootstrap.com/img/new/standard/nature/111.webp"
+              src={thumbnail}
               alt="#"
               href="thumbnail"
               className="img-fluid"
             />
           </div>
           <div className="card-body text-center">
-            <h4 className="card-title">Title</h4>
+            <h4 className="card-title">{title}</h4>
           </div>
           <div className="card-body">
             <p className="card-text">
               <ul>
+                <li>By : {uploadedBy}</li>
+                <li>Topic : {topic}</li>
                 <li>
-                  <p>By : </p>
-                </li>
-                <li>
-                  <p>Category : </p>
-                </li>
-                <li>
-                  <p>Score : </p>
+                  Score :
+                  <ReactStars
+                    size={"25px"}
+                    readonly={true}
+                    transition={true}
+                    initialValue={totalRating}
+                  />
                 </li>
               </ul>
             </p>
           </div>
           <div className="card-footer text-center">
-            <button type="button" className="btn btn-primary">
-              Video
+            <button
+              type="button"
+              onClick={() => {
+                window.open(videoLink);
+              }}
+              className="btn btn-primary"
+            >
+              Video Link
             </button>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
