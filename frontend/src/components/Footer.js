eda3430858b482b7503d7e33718116e2ec283df5
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-01 17:35:52 +0530

    Fixed Some Bugs in Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,41 @@
+function Footer() {
+  return (
+    <footer className="bg-dark text-center text-white">
+      <div className="container p-4 pb-0">
+        <section className="mb-4">
+          <a
+            className="btn btn-outline-light btn-floating m-1"
+            href="https://www.linkedin.com/in/prateek-kumar-2252ab1a3/"
+            role="button"
+          >
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+
+          <a
+            className="btn btn-outline-light btn-floating m-1"
+            href="https://github.com/Enforc3rr"
+            role="button"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+        </section>
+      </div>
+
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        {`Made By `}
+        <a
+          className="text-white"
+          href="https://steamcommunity.com/id/Enforc3rr/"
+        >
+          {`Enforc3rr `}
+          <i class="fas fa-headset"></i>
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
