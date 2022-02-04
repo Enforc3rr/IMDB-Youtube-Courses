d1a1198b44177425aaaa00b74dc3fdda95d433db
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-04 19:07:30 +0530

    Made Axios support for proper sign ups and changed how UI reacts after sign up

--- frontend/src/components/SignupPage.js
+++ frontend/src/components/SignupPage.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "../App.css";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Axios from "axios";
 
 function SignupPage() {
   const [isPageLoading, setIsPageLoading] = useState(true);
@@ -13,12 +14,52 @@ function SignupPage() {
   const [passwordDetailsText, setPasswordDetailsText] = useState(
     "Enter A Password That's Longer Than 6 Characters"
   );
+  const [waitingForFormResponse, setWaitingForFormResponse] = useState(false);
+  const [displayingResponse, setDisplayingResponse] = useState(false);
+  const [responseText, setReponseText] = useState("");
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsPageLoading(false);
-    }, 1000);
+    setIsPageLoading(false);
   }, []);
+
+  const signupButtonHandle = () => {
+    setWaitingForFormResponse(true);
+    const dataToBeSent = {
+      name,
+      username,
+      email,
+      password,
+    };
+    const config = {
+      headers: {
+        "content-type": "application/json",
+      },
+    };
+    Axios.post(
+      "http://localhost:8000/api/v1/user/usersignup",
+      dataToBeSent,
+      config
+    )
+
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        setWaitingForFormResponse(false);
+        if (res.data.userDataAdded) {
+          setDisplayingResponse(true);
+          setReponseText(
+            `${username} , You have been successfully Added To The Web App`
+          );
+        }
+      })
+      .catch((error) => {
+        setWaitingForFormResponse(false);
+        setDisplayingResponse(true); // error.response.data || Still have to optimize my API for a more specific response...!!!
+        setReponseText(
+          "You have used a username or email , that's already present in database"
+        );
+      });
+  };
 
   const checkPasswordFunc = (e) => {
     setPassword(e.target.value);
@@ -65,12 +106,98 @@ function SignupPage() {
           backgroundColor: "rgba(255,255,255,0)",
           color: "black",
         }}
+        onClick={signupButtonHandle}
         className="btnSignup"
       >
         Signup
       </button>
     );
   };
+
+  const signupForm = () => {
+    return (
+      <>
+        <p
+          className="h1"
+          style={{
+            color: "beige",
+            textDecoration: "underline",
+            textDecorationColor: "#800",
+          }}
+        >
+          Create An Account
+        </p>
+
+        <input
+          className="inputSignup"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          className="inputSignup"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          className="inputSignup"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="inputSignup"
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => checkPasswordFunc(e)}
+        />
+        <div
+          className="d-flex justify-content-end text-muted"
+          style={{ width: "70%" }}
+        >
+          {passwordDetailsText}
+        </div>
+
+        {passwordCheck ? enabledButton() : disabledButton()}
+      </>
+    );
+  };
+
+  const signupResponse = () => {
+    return (
+      <>
+        <p
+          className="h1"
+          style={{
+            color: "beige",
+            textDecoration: "underline",
+            textDecorationColor: "#800",
+          }}
+        >
+          {responseText}
+        </p>
+        <button
+          style={{
+            borderRadius: "8px",
+            fontSize: "1.5em",
+            margin: "2%",
+            fontWeight: "bold",
+            borderBlockColor: "#800",
+            borderColor: "#800",
+            backgroundColor: "rgba(255,255,255,0)",
+            color: "black",
+          }}
+          onClick={() => setDisplayingResponse(false)}
+          className="btnSignup"
+        >
+          Back To Form
+        </button>
+      </>
+    );
+  };
+
   const Signup = () => {
     return (
       <>
@@ -90,55 +217,15 @@ function SignupPage() {
           >
             Youtube Courses IMDB
           </p>
-          <div className="d-flex justify-content-center align-items-center flex-column shadow p-3 containerSignup">
-            <p
-              className="h1"
-              style={{
-                color: "beige",
-                textDecoration: "underline",
-                textDecorationColor: "#800",
-              }}
-            >
-              Create An Account
-            </p>
-
-            <input
-              className="form-control"
-              placeholder="Name"
-              className="inputSignup"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <input
-              className="form-control"
-              placeholder="Username"
-              className="inputSignup"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              className="form-control"
-              placeholder="Email"
-              className="inputSignup"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              className="form-control"
-              placeholder="Password"
-              className="inputSignup"
-              type="password"
-              value={password}
-              onChange={(e) => checkPasswordFunc(e)}
-            />
-            <div
-              className="d-flex justify-content-end text-muted"
-              style={{ width: "70%" }}
-            >
-              {passwordDetailsText}
-            </div>
-
-            {passwordCheck ? enabledButton() : disabledButton()}
+          <div
+            className="d-flex justify-content-center align-items-center flex-column shadow p-3 containerSignup"
+            style={{ height: "60vh" }}
+          >
+            {waitingForFormResponse
+              ? loadingPage()
+              : displayingResponse
+              ? signupResponse()
+              : signupForm()}
           </div>
         </div>
         <Footer />
@@ -153,7 +240,7 @@ function SignupPage() {
       >
         <div
           className="spinner-grow"
-          style={{ width: "30vh", height: "30vh" }}
+          style={{ width: "30vh", height: "30vh", color: "#800" }}
         ></div>
       </div>
     );
