4be3db002c74b954ab4fa311490345192debf8e8
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-06 21:43:54 +0530

    Added Animate.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+import "animate.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
