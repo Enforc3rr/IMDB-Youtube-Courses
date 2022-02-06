da93845c752c603529a1dadc12378128663792e5
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-06 21:43:39 +0530

    Added a header in response to expose authorization .

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -2,6 +2,7 @@ const cors = async (req,res,next)=>{
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "*");
         res.header("Access-Control-Allow-Headers",  "*");
+        res.header("Access-Control-Expose-Headers", "Authorization")
         next();
 }
 
