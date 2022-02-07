972853a223e94496edc990c2546d9ce693da6c25
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-07 21:15:45 +0530

    Added Logout button

---
+++ frontend/src/components/Logout.js
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { LoginContext } from "../helper/LoginContext";
+
+function Logout() {
+  const { isUserLoggedIn, setIsUserLoggedIn } = useContext(LoginContext);
+  const navigate = useNavigate();
+  const logoutHandle = () => {
+    setIsUserLoggedIn(false);
+    localStorage.removeItem("tokenYoutubeIMDB");
+    navigate("/");
+  };
+  return (
+    <div>
+      <button onClick={logoutHandle}>logout</button>
+    </div>
+  );
+}
+
+export default Logout;
