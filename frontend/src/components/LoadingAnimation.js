c05f7f5e9f21ec49005d0bec2acabdf73e9b8762
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-13 04:01:03 +0530

    Made A separate file to store Loading Animation

---
+++ frontend/src/components/LoadingAnimation.js
@@ -0,0 +1,15 @@
+function LoadingAnimation() {
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ width: "100wv", height: "100vh" }}
+    >
+      <div
+        className="spinner-grow"
+        style={{ width: "20vh", height: "20vh" }}
+      ></div>
+    </div>
+  );
+}
+
+export default LoadingAnimation;
