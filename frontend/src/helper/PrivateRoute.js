6de56c05d79cc68ea60580904e1781f8a2f603c2
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-14 22:05:19 +0530

    Added Private Route Helper

--- frontend/src/helper/PrivateRoute.js
+++ frontend/src/helper/PrivateRoute.js
@@ -1,10 +1,16 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { LoginContext } from "./LoginContext";
 
-export default function PrivateRoute(props) {
+const PrivateRoute = (props) => {
+  console.log(props);
   const { isUserLoggedIn, doneloading } = useContext(LoginContext);
-  if (!isUserLoggedIn && doneloading) return <Navigate to="/signin" />;
-  if (doneloading) return props.children;
-  return null;
-}
+  if ((props.reversed ? isUserLoggedIn : !isUserLoggedIn) && doneloading)
+    return <Navigate to={props.to ?? "/"} />; // ?? basically means if value is null then assign it the value on right of " ?? "
+  if (doneloading) {
+    return <React.Fragment>{props.children}</React.Fragment>;
+  }
+  return <React.Fragment></React.Fragment>;
+};
+
+export default PrivateRoute;
