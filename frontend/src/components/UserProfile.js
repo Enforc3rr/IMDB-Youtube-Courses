7d6df00bc45e06bf0645d272ec4c54c6dc94ad25
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-14 22:06:03 +0530

    Improved UserProfile UI.

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -2,13 +2,16 @@ import Axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { LoginContext } from "../helper/LoginContext";
+import "../App.css";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function UserProfile() {
   const { isUserLoggedIn, setIsUserLoggedIn } = useContext(LoginContext);
   const [loggedInUser, setLoggedInUser] = useState("");
   let { username } = useParams();
 
-  const [user, setUser] = useState("Code016");
+  const [user, setUser] = useState(username);
 
   useEffect(() => {
     if (localStorage.getItem("tokenYoutubeIMDB") && isUserLoggedIn) {
@@ -30,13 +33,42 @@ function UserProfile() {
 
   return (
     <>
-      {loggedInUser === user ? (
-        <>
-          Welcome To Your Dashboard <button>Logout</button>{" "}
-        </>
-      ) : (
-        <>User Profile </>
-      )}
+      <Navbar login={{ isUserLoggedIn, setIsUserLoggedIn }} />
+      <div className="containerUser" style={{ minHeight: "100vh" }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-lg-12 col-md-12 d-flex justify-content-center align-items-center flex-column p-5">
+              <img
+                src="https://picsum.photos/300/300"
+                style={{ width: "250px", height: "250px" }}
+                alt=""
+                className="img-fluid rounded-circle shadow-lg mb-2"
+              />
+              <h1
+                className="display-3 mt-2 mb-4"
+                style={{
+                  color: "beige",
+                  textDecoration: "underline",
+                  textDecorationColor: "#800",
+                }}
+              >
+                {username}
+              </h1>
+              <button className="btn btn-outline-danger mt-1">Logout</button>
+            </div>
+            <div className="col-sm-12 col-lg-12 col-md-12">
+              <div className="row">
+                <div className="col-sm-12 col-lg-12 col-md-12 text-center">
+                  <h3 className="mt-1 mb-4" style={{ color: "beige" }}>
+                    Last Three Posts
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
