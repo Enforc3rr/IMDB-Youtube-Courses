7800ee7cba9b04606982e125d8ed1d3bdb71b489
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-17 04:43:32 +0530

    Added user authentication part in VideoAvailabilityCheck.js

--- frontend/src/components/VideoAvailabilityCheck.js
+++ frontend/src/components/VideoAvailabilityCheck.js
@@ -1,19 +1,47 @@
-import axios from "axios";
-import { useState } from "react";
+import Axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import VideoDetailsForm from "./VideoDetailsForm";
+import "../App.css";
+import { URL } from "./Data";
+import { LoginContext } from "../helper/LoginContext";
 
 function VideoAvailabilityCheck() {
   const [videoPresent, setVideoPresent] = useState(null);
   const [initialCheck, setInitialCheck] = useState(false);
   const [videoUrl, setVideoUrl] = useState("");
   const [next, setNext] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState();
+  const { isUserLoggedIn, setIsUserLoggedIn } = useContext(LoginContext);
+
+  useEffect(() => {
+    if (isUserLoggedIn && localStorage.getItem("tokenYoutubeIMDB")) {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("tokenYoutubeIMDB")}`,
+        },
+      };
+      Axios.get("http://localhost:8000/api/v1/user/userdetails", config)
+        .then((res) => {
+          setLoggedInUser(res.data.username);
+        })
+        .catch((err) => {
+          setIsUserLoggedIn(false);
+        });
+    }
+  }, []);
 
   const checkVideoAvailabilty = async () => {
     setInitialCheck(true);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("tokenYoutubeIMDB")}`,
+      },
+    };
     //To add video url verification check .
     const videoID = videoUrl.split("watch?v=")[1].split("&")[0];
-    const response = await axios.get(
-      `http://localhost:8000/api/v1/video/videocheck?vid=${videoID}`
+    const response = await Axios.get(
+      `${URL}/api/v1/video/videocheck?vid=${videoID}`,
+      config
     );
     if (response.data.isVideoPresent) {
       setVideoPresent(true);
@@ -25,58 +53,61 @@ function VideoAvailabilityCheck() {
   const videoDetails = () => {
     setNext(true);
   };
+
   return (
-    <div>
-      {!next ? (
-        <div
-          className="container d-flex justify-content-center align-items-center"
-          style={{ border: "2px solid black", minHeight: "100vh" }}
-        >
-          <div className="form-group row shadow-lg" style={{ width: "100%" }}>
-            <div className="text-center col-sm-12 col-md-12 col-lg-12">
-              <h2 className="display-4">
-                <u>Add Video Details</u>
-              </h2>
-            </div>
-            <div className="col-sm-12 col-md-4 col-lg-4 pl-5 pt-1">
-              <label htmlFor="youtubeLink" className="display-4">
-                Video Link
-              </label>
-            </div>
-            <div className="col-sm-12 col-md-8 col-lg-8 mt-2">
-              <input
-                type="text"
-                id="youtubeLink"
-                className="form-control"
-                style={{ height: "75%" }}
-                value={videoUrl}
-                onChange={(e) => setVideoUrl(e.target.value)}
-              />
-            </div>
-            <div className="col-12 text-center">
-              {!initialCheck ? (
-                <button
-                  className="btn btn-primary"
-                  onClick={checkVideoAvailabilty}
-                >
-                  Check Availability
-                </button>
-              ) : !videoPresent ? (
-                <button className="btn btn-green" onClick={videoDetails}>
-                  Next
-                </button>
-              ) : (
-                <button className="btn btn-danger" disabled>
-                  Video is already present
-                </button>
-              )}
+    <>
+      <div className="containerUser" style={{ minHeight: "100vh" }}>
+        {!next ? (
+          <div
+            className="container d-flex justify-content-center align-items-center"
+            style={{ minHeight: "100vh" }}
+          >
+            <div className="form-group row shadow-lg" style={{ width: "100%" }}>
+              <div className="text-center col-sm-12 col-md-12 col-lg-12">
+                <h2 className="display-4">
+                  <u>Add Video Details</u>
+                </h2>
+              </div>
+              <div className="col-sm-12 col-md-4 col-lg-4 pl-5 pt-1">
+                <label htmlFor="youtubeLink" className="display-4">
+                  Video Link
+                </label>
+              </div>
+              <div className="col-sm-12 col-md-8 col-lg-8 mt-2">
+                <input
+                  type="text"
+                  id="youtubeLink"
+                  className="form-control"
+                  style={{ height: "75%" }}
+                  value={videoUrl}
+                  onChange={(e) => setVideoUrl(e.target.value)}
+                />
+              </div>
+              <div className="col-12 text-center">
+                {!initialCheck ? (
+                  <button
+                    className="btn btn-primary"
+                    onClick={checkVideoAvailabilty}
+                  >
+                    Check Availability
+                  </button>
+                ) : !videoPresent ? (
+                  <button className="btn btn-green" onClick={videoDetails}>
+                    Next
+                  </button>
+                ) : (
+                  <button className="btn btn-danger" disabled>
+                    Video is already present
+                  </button>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <VideoDetailsForm videoUrl={`${videoUrl}`} />
-      )}
-    </div>
+        ) : (
+          <VideoDetailsForm videoUrl={`${videoUrl}`} user={loggedInUser} />
+        )}
+      </div>
+    </>
   );
 }
 
