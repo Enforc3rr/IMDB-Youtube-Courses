b17e52d5efff5549a87c86e83291ee88b96e13e9
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-18 03:49:20 +0530

    removed Logout Button

--- frontend/src/components/Logout.js
+++ frontend/src/components/Logout.js
@@ -1,21 +1,16 @@
-import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { LoginContext } from "../helper/LoginContext";
+// import { useContext, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { LoginContext } from "../helper/LoginContext";
 
-function Logout() {
-  const { isUserLoggedIn, setIsUserLoggedIn } = useContext(LoginContext);
-  const navigate = useNavigate();
+// function Logout() {
+//   const { isUserLoggedIn, setIsUserLoggedIn } = useContext(LoginContext);
+//   const navigate = useNavigate();
 
-  const logoutHandle = () => {
-    setIsUserLoggedIn(false);
-    localStorage.removeItem("tokenYoutubeIMDB");
-    navigate("/");
-  };
-  return (
-    <div>
-      <button onClick={logoutHandle}>logout</button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <button onClick={logoutHandle}>logout</button>
+//     </div>
+//   );
+// }
 
-export default Logout;
+// export default Logout;
