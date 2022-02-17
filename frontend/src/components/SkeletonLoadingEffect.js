c422a91540d07506d4984fbcc1ec91b5579b326e
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-18 03:43:33 +0530

    Made Skeleton loading a separate Function

---
+++ frontend/src/components/SkeletonLoadingEffect.js
@@ -0,0 +1,18 @@
+function SkeletonLoadingEffect() {
+  return (
+    <div className="ph-item">
+      <div className="ph-col-12">
+        <div className="ph-picture"></div>
+        <div className="ph-row">
+          <div className="ph-col-12 big"></div>
+          <div className="ph-col-12 empty big"></div>
+          <div className="ph-col-12"></div>
+          <div className="ph-col-12 empty big"></div>
+          <div className="ph-col-12 big"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SkeletonLoadingEffect;
