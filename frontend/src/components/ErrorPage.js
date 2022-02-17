95101888b4c5b5de7e52cdf7278f9a247b633159
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-02-18 03:44:24 +0530

    Updated ErrorPage.js UI

--- frontend/src/components/ErrorPage.js
+++ frontend/src/components/ErrorPage.js
@@ -1,5 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  let navigate = useNavigate();
+  return (
+    <div className="containerUser ">
+      <div
+        className="container d-flex justify-content-center align-items-center flex-column"
+        style={{ minHeight: "100vh" }}
+      >
+        <h1
+          style={{
+            color: "beige",
+            textDecoration: "underline",
+            textDecorationColor: "#800",
+          }}
+        >
+          Sorry , This Page Does Not Exists
+        </h1>
+        <h4
+          style={{
+            color: "beige",
+            textDecoration: "underline",
+            textDecorationColor: "#800",
+          }}
+        >
+          Redirecting you to Homepage in 3seconds
+        </h4>
+        {setTimeout(() => {
+          navigate("/");
+        }, 3000)}
+      </div>
+    </div>
+  );
 }
 
 export default ErrorPage;
